37100e551c8a5387c52f57c4492bbc6ce93711b7
role-nzo <51129456+role-nzo@users.noreply.github.com>
2019-05-28 18:08:14 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Instascan</title>
+    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+  </head>
+  <body>
+	<iframe src="https://schmich.github.io/instascan/" width="4000" height="4000"></iframe>
+  </body>
+</html>
